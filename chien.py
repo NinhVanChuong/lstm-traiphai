5c0279aaddf1b4cfb7a4b487321d8c7b695dfa8e
unknown <chuongnv.nd@gmail.com>
2022-03-18 22:41:59 +0700

    Ba cu chi

---
+++ chien.py
@@ -0,0 +1,56 @@
+import numpy as np
+import pandas as pd
+
+from keras.layers import LSTM, Dense,Dropout
+from keras.models import Sequential
+
+from sklearn.model_selection import train_test_split
+
+# Đọc dữ liệu
+quaytrai_df = pd.read_csv("quaytrai.txt")
+quayphai_df = pd.read_csv("quayphai.txt")
+xoaytay_df = pd.read_csv("xoaytay1.txt")
+
+X = []
+y = []
+no_of_timesteps = 10
+
+dataset =quaytrai_df.iloc[:,1:].values
+n_sample = len(dataset)
+for i in range(no_of_timesteps, n_sample):
+    X.append(dataset[i-no_of_timesteps:i,:])
+    y.append([1,0,0])
+
+dataset = quayphai_df.iloc[:,1:].values
+n_sample = len(dataset)
+for i in range(no_of_timesteps, n_sample):
+    X.append(dataset[i-no_of_timesteps:i,:])
+    y.append([0,0,1])
+
+dataset = xoaytay_df.iloc[:,1:].values
+n_sample = len(dataset)
+for i in range(no_of_timesteps, n_sample):
+    X.append(dataset[i-no_of_timesteps:i,:])
+    y.append([0,1,0])
+
+X, y = np.array(X), np.array(y)
+print(X.shape, y.shape)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+model  = Sequential()
+model.add(LSTM(units = 50, return_sequences = True, input_shape = (X.shape[1], X.shape[2])))
+model.add(Dropout(0.2))
+model.add(LSTM(units = 50, return_sequences = True))
+model.add(Dropout(0.2))
+model.add(LSTM(units = 50, return_sequences = True))
+model.add(Dropout(0.2))
+model.add(LSTM(units = 50))
+model.add(Dropout(0.2))
+model.add(Dense(units = 3, activation="softmax"))
+model.compile(optimizer="adam", metrics = ['accuracy'], loss = "binary_crossentropy")
+
+model.fit(X_train, y_train, epochs=16, batch_size=32,validation_data=(X_test, y_test))
+model.save("model3.h5")
+
+
